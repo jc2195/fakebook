584304076570d52f9a3a314c024272dcc511a326
Kuba <k.ciukiewicz@gmail.com>
2020-09-08 02:47:31 +0100

    Changed SendGrid to Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :user_name => ENV['app183651452@heroku.com'],
-  :password => ENV['ss5dwwju3204'],
+  :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+  :password => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain => 'https://whispering-mesa-77843.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :address => ENV['MAILGUN_SMTP_SERVER'],
+  :port => ENV['MAILGUN_SMTP_PORT'],
   :authentication => :plain,
   :enable_starttls_auto => true
 }
